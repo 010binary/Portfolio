cb28bf28748d23aea119ff7ecd3c95348d7d7896
010binary <augustine32fg@gmail.com>
2024-03-03 15:59:59 +0100

    stop point

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -6,12 +6,25 @@ const About = () => {
   return (
     <div className="about__section">
       <img src={AboutMe} alt="" />
-      <div className="about__section--leftside">
-        <img src={AboutmeImg} alt="" />
-      </div>
-      <div className="about__section--rightside">
-        <img src={Lightbulb} alt="Creative Lightbulb design" />
-        <img src={Music} alt="Creative Music design" />
+      <div className="about__section-content">
+        <div className="about__section--leftside">
+          <img src={AboutmeImg} alt="" />
+        </div>
+        <div className="about__section--rightside">
+          <img src={Lightbulb} alt="Creative Lightbulb design" className="light"/>
+          <h2>
+            <span className="block">Let’s get know </span>
+            <span className="block">
+              <span className="blue-text">about me </span>closer
+            </span>
+          </h2>
+          <p className="about__section--description">
+            Aaronn is a New York-based visual designer focusing on branding and
+            visual identity. Her portfolio showcases her wide range of work,
+            spanning books, posters and web design. <b>Read more</b>
+          </p>
+          <img src={Music} alt="Creative Music design" className="music"/>
+        </div>
       </div>
     </div>
   );
