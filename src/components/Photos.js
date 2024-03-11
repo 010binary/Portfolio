d99448d4ae1eeca47854e270bc6dc82465c88019
010binary <augustine32fg@gmail.com>
2024-03-11 21:52:19 +0100

    my personal images added

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -1,5 +1,5 @@
-import HeroImg from "../imgs/Photos/Hero-image.svg";
-import AboutmeImg from "../imgs/Photos/About-me.svg";
+import HeroImg from "../imgs/Photos/Hero-image.webp";
+import AboutmeImg from "../imgs/Photos/About-me.webp";
 import Portfolio1 from "../imgs/Photos/Portfolio-item1.webp";
 import Portfolio2 from "../imgs/Photos/Portfolio-item2.webp";
 import Portfolio3 from "../imgs/Photos/Portfolio-item3.webp";
