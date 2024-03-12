2c6201ea1381e86eced0d59234c6a175246206e5
010binary <augustine32fg@gmail.com>
2024-03-12 04:48:25 +0100

    Nav bar working

--- src/components/navbar/NarBar.jsx
+++ src/components/navbar/NarBar.jsx
@@ -1,15 +1,42 @@
 import { Logo, Download } from "../Icons";
 import { NavLink } from "react-router-dom";
 import "./NarBar.scss";
+import { useRef, useEffect } from "react";
 
 const NarBar = () => {
+  
   const downloadResume = () => {
     const resumeUrl =
       "https://drive.google.com/file/d/1-sgzJU7omKHt4MW1yRfx3BXUJPvAbrXE/view?usp=drive_link";
     window.open(resumeUrl, "_blank"); // Open in a new tab
   };
+
+
+  const navbarRef = useRef(null); // Create a ref to access the navbar element
+  let prevScrollpos = window.pageYOffset;
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+      const { current } = navbarRef; // Get the navbar element
+
+      if (prevScrollpos > currentScrollPos) {
+        current.style.top = "0";
+      } else {
+        current.style.top = "-100px";
+      }
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      prevScrollpos = currentScrollPos;
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll); // Clean up on unmount
+    };
+  }, []); // Empty dependency array to run only once
+
   return (
-    <nav className="container">
+    <nav className="container" ref={navbarRef}>
       <div className="logo">
         <NavLink to="/home">
           <img src={Logo} alt="My Logo" />
