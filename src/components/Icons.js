77743f5c5cf169ab09d239201906c4366690929d
010binary <augustine32fg@gmail.com>
2024-03-13 18:23:34 +0100

    deploying

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -6,7 +6,7 @@ import Github from "../imgs/icons/Github-icon.svg";
 import GithubFooter from "../imgs/icons/Github-footer.svg";
 import Instagram from "../imgs/icons/Instagram-icon.svg";
 import InstagramFooter from "../imgs/icons/Instragram-footer.svg";
-import Leftarrow from "../imgs/icons/Left.svg";
+import Leftarrow from "/src/imgs/icons/Left.svg";
 import Lightbulb from "../imgs/icons/Lightbulb-icon.svg";
 import Linkedin from "../imgs/icons/Linkedin-icon.svg";
 import LinkedinFooter from "../imgs/icons/Linkedin-footer.svg";
--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -2,11 +2,26 @@ import "./Project.scss";
 import PropTypes from "prop-types";
 import { Portfolio1, Portfolio2, Portfolio3 } from "../Photos";
 import { Projects, Leftarrow, Right, Design } from "../Icons";
-import { useState } from "react";
 import { Link } from "react-router-dom";
-// import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import BaseSkeleton from "../skeleton/BaseSkeleton";
+import SkeletonCard from "../skeleton/SkeletonCard";
 
 const Project = ({ projectsToShow, title }) => {
+  const [loading, setLoading] = useState(true);
+  const [loadingImages, setLoadingImages] = useState(true);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setLoading(false);
+      setLoadingImages(false);
+    }, 5000);
+
+    // Clear timeout on component unmount
+    return () => clearTimeout(timeout);
+  }, []); // Empty dependency array ensures the effect runs only once after the initial render
+
   const displayProject = [
     {
       name: "Agency website.",
@@ -40,8 +55,6 @@ const Project = ({ projectsToShow, title }) => {
     },
   ];
 
-  const [currentIndex, setCurrentIndex] = useState(0);
-
   const handleLeftClick = () => {
     setCurrentIndex(Math.max(currentIndex - 1, 0)); // Clamp to prevent negative index
   };
@@ -55,26 +68,42 @@ const Project = ({ projectsToShow, title }) => {
   const renderProject = displayProject
     .slice(currentIndex, currentIndex + projectsToShow)
     .map((project) => (
-      <div key={project.name} className="project__section--content--projects">
-        <section className="image-project">
-          <img src={project.img} alt={`project image for ${project.name}`} />
-          <div className="overlay">
-            <Link to="/project/details" className="text">
-              Visit
-            </Link>
-          </div>
-        </section>
-        <section className="about-project">
-          <p className="project-name">{project.name}</p>
-          <section className="project-decription">
-            <p className="Project-languages">
-              {project.languages.map((language, index) => (
-                <span key={index}>{language}</span>
-              ))}
-            </p>
-          </section>
-        </section>
-      </div>
+      <>
+        {loadingImages ? (
+          <>
+            <SkeletonCard />
+          </>
+        ) : (
+          <>
+            <div
+              key={project.name}
+              className="project__section--content--projects"
+            >
+              <section className="image-project">
+                <img
+                  src={project.img}
+                  alt={`project image for ${project.name}`}
+                />
+                <div className="overlay">
+                  <Link to="/project/details" className="text">
+                    Visit
+                  </Link>
+                </div>
+              </section>
+              <section className="about-project">
+                <p className="project-name">{project.name}</p>
+                <section className="project-decription">
+                  <p className="Project-languages">
+                    {project.languages.map((language, index) => (
+                      <span key={index}>{language}</span>
+                    ))}
+                  </p>
+                </section>
+              </section>
+            </div>
+          </>
+        )}
+      </>
     ));
 
   return (
@@ -87,23 +116,37 @@ const Project = ({ projectsToShow, title }) => {
           className="project-Top-design"
         />
         <div className="project__section--header">
-          {title}
-          <div className="project__section--header--buttons">
-            <button
-              className="left-btn"
-              disabled={currentIndex === 0}
-              onClick={handleLeftClick}
+          {loading ? (
+            <div
+              style={{
+                width: "30%",
+              }}
             >
-              <img src={Leftarrow} alt="An arrow indicating left scroll" />
-            </button>
-            <button
-              className="right-btn"
-              disabled={currentIndex === displayProject.length - projectsToShow}
-              onClick={handleRightClick}
-            >
-              <img src={Right} alt="An arrow indicating Right scroll" />
-            </button>
-          </div>
+              <BaseSkeleton type="text" />
+            </div>
+          ) : (
+            <>
+              {title}
+              <div className="project__section--header--buttons">
+                <button
+                  className="left-btn"
+                  disabled={currentIndex === 0}
+                  onClick={handleLeftClick}
+                >
+                  <img src={Leftarrow} alt="An arrow indicating left scroll" />
+                </button>
+                <button
+                  className="right-btn"
+                  disabled={
+                    currentIndex === displayProject.length - projectsToShow
+                  }
+                  onClick={handleRightClick}
+                >
+                  <img src={Right} alt="An arrow indicating Right scroll" />
+                </button>
+              </div>
+            </>
+          )}
         </div>
         <div className="project__section--content">{renderProject}</div>
       </div>
